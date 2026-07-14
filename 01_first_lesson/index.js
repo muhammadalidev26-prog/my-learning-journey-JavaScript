ede1a7bb350e955ef12dbadd0c1563cb78fbf985
muhammadalidev26-prog <muhammadali.dev26@gmail.com>
2026-07-14 09:32:01 +0500

    Add files via upload

---
+++ 01_first_lesson/index.js
@@ -0,0 +1,11 @@
+
+
+// 1. Select the button element using its ID
+const button = document.getElementById("helloButton");
+
+// 2. Add the click event listener and trigger the alert function
+button.addEventListener("click", function() {
+    alert("Hello Adones!");
+});
+
+console.log("NOTE: Don't paste any piece of code in order hack or steal information")
